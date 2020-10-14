3fb2216a1e6b663d48df63dbe276870de8b3303d
Conan-One <61743295+theConanEdogawa@users.noreply.github.com>
2020-10-14 21:15:57 +0800

    The first script in life!

---
+++ Run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+log=/home/art/logs/Run-ART-based-on-load.log
+[[ ! -f $log ]] && { mkdir -p $(dirname $log) ; touch $log ; }
+
+preset_maximum_load=2
+
+#抓取系统1分钟平均负载
+average=$(uptime | awk '{print $10}' | cut -f 1 -d ",")
+
+#抓取系统1分钟平均负载并取个位整数
+average_int=$(uptime | awk '{print $10}' | cut -f 1 -d ".")
+
+#当前负载小于2时，运行ART，否则不运行
+if (("$average_int" < "$preset_maximum_load")); then
+    echo -e "$(date "+%Y.%m.%d %H:%M:%S") System load: $average" >> $log
+    echo "The ART was executed successfully" >> $log
+    /usr/local/bin/autoremove-torrents --conf=/home/art/config.yml --log=/home/art/logs
+else
+    echo -e "$(date "+%Y.%m.%d %H:%M:%S") System load: $average" >> $log
+    echo "The ART was not executed because the current load is greater than the preset." >> $log
+fi
